330b56f76d200e7ed1931372a8e5925b196faf26
SoohoPark <clapp301@gmail.com>
2025-03-26 17:55:25 +0900

    initial commit

---
+++ src/main/java/com/vemo/VemoApplication.java
@@ -0,0 +1,13 @@
+package com.vemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vemo/VemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
